148a48ee00c1c8b181b24f44ca6a53f83c83423f
TAWANY OLIVEIRA <tawany.oliveira@fatec.sp.gov.br>
2024-09-02 12:20:54 -0300

    Aula 02/09 - estrutura de entidades, categoria e produtos

---
+++ HelpStockApp/HelpStockApp.Domain/Entities/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelpStockApp.Domain.Entities
+{
+    internal class Category : Entity
+    {
+        public string name { get; set; }
+
+        public Category(string name)
+        {
+            this.name = name;
+        }
+        public Entity
+    }
+}
